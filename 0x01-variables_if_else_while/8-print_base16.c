21adb14cdf534b55e25c0011919e5ba5a7938cd9
AllwellUmoh <allwellumoh1@gmail>
2022-10-14 19:55:40 -0700

    Write a program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alnum;
+	int numal;
+
+	for(numal = 0; numal < 10; numal++)
+	putchar(numal + '0');
+
+	for(alnum = 'a'; alnum <= 'f'; alnum++)
+	putchar(alnum);
+	putchar('\n');
+
+	return (0);
+}
