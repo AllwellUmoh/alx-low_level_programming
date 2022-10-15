c14c1a38a56f4b0d0a603f10a84433c5ea06798f
AllwellUmoh <allwellumoh1@gmail>
2022-10-14 20:18:15 -0700

    Write a program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Task Seven
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	putchar(num + '0');
+	if (number < 9)
+	{
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
