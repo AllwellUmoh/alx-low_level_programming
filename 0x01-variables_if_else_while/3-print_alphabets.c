021dda107fc0b30eacf9e0851d3d375d75401ec7
AllwellUmoh <allwellumoh1@gmail>
2022-10-14 17:45:41 -0700

    Putchar three 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                                                                                      
-                                                                                                                                        
+#include <stdio.h>
 
 /**                                                                                                                                     
- *main - Entry point                                                                                                                    
+ *main - Entry point
  *
- *Return: Always 0 (Success)                                                                                                            */
+ * Always 0 (Success)
+ */
 
 int main(void)
 {
