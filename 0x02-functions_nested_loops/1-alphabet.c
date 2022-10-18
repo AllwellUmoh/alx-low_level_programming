d80b58314944734c1a94673ba59a83c4ed4429c7
AllwellUmoh <allwellumoh1@gmail>
2022-10-18 16:09:16 -0700

    Update on alphabe and task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: 0
+ * main - Write a function that prints the alphabet, in lowercase
+ * 
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {
 	char alpha;
+	
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Write a function that prints 10 times the alphabet, in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char lowers;
+	int i;
+
+	for (lowers = 'a'; lowers <= 'z'; lowers+++)
+	{
+		for (i = 0; i <= 10; i++)
+		{
+			putchar(lowers);
+		}
+		putchar("\n");
+	}
+}
+
