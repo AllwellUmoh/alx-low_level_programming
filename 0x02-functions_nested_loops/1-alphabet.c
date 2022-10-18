25754527f4212b9580f7d7dd34abff0b49c81111
AllwellUmoh <allwellumoh1@gmail>
2022-10-18 16:48:39 -0700

    task one refix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * main - Write a function that prints the alphabet, in lowercase
+ * print_alphabet - Prints the alphabet in lowercase
  * 
  * Return: Always 0 (Success)
  */
-
 void print_alphabet(void)
 {
 	char alpha;
-	
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
