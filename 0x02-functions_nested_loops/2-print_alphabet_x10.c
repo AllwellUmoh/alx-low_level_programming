3050b218f2a123feb217cbff3173646de5398b66
AllwellUmoh <allwellumoh1@gmail>
2022-10-18 16:42:51 -0700

    great

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,20 @@
 #include "main.h"
 
 /**
- * main - Write a function that prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 -  prints the alphabet x10
  *
- * Return: 0 (Success)
  */
 
 void print_alphabet_x10(void)
 {
 	int alpha;
 	int alpha2;
-	
 	for (alpha = 0; alpha <= 9; alpha++)
 	{
-	for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
-	{
-	_putchar(alpha2);
-	}
-	_putchar('\n');
+		for (alpha2 = 'a'; alpha2 <= 'z'; alpha2++)
+		{
+			_putchar(alpha2);
+		}
+		_putchar('\n');
 	}
 }
