a5ee5c20a560c588567869e29355e71b82316925
AllwellUmoh <allwellumoh1@gmail>
2022-10-18 17:56:05 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * _abs - Computes the absolute value of an integer
+ * @n: number being tested
  * Return: Always 0.
  */
 
 int main(void)
 {
-	int r;
-
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
 	return (0);
 }
