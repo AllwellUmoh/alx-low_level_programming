a5743bd533f5696f3d495bf536d94d81283334ee
AllwellUmoh <allwellumoh1@gmail>
2022-10-20 09:01:07 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,10 @@
 
 int main(void)
 {
-/**      int i;*/
+/*      int i;*/
 
 	printf("Infinite loop incoming :(\n");
+
 /**
  * 	i = 0;
  *	while (i < 10)
@@ -17,6 +18,7 @@ int main(void)
  *	putchar(i);
  *	}
  */
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
