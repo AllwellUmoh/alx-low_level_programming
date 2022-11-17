df7f425c8b908da8c64f0c093d7c998049ce5012
AllwellUmoh <allwellumoh1@gmail>
2022-11-16 19:28:50 -0800

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - entry piint from main
+ * @name: String of name
+ * @f:	Entry to another function
+ */
+
+void print_name(char *name, void (*f)(char *n))
+{
+	if(name == NULL || f == NULL)
+		return;
+	f(name);
+}
